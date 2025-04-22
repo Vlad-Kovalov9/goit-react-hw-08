2ba71626b4f634516b9c143b2200b9a7a5bee819
Vlad <nowornever969@gmail.com>
2025-04-22 15:47:37 +0300

    finish

---
+++ src/components/App.jsx
@@ -0,0 +1,70 @@
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, lazy } from "react";
+import { refreshUser } from "../redux/auth/operations";
+import { selectIsRefreshing } from "../redux/auth/selectors";
+import { Route, Routes } from "react-router-dom";
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const RegistrationPage = lazy(() =>
+  import("../pages/RegistrationPage/RegistrationPage")
+);
+const LoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
+const ContactsPage = lazy(() => import("../pages/ContactsPage/ContactsPage"));
+import { Layout } from "../components/Layout/Layout";
+import { RestrictedRoute } from "../components/RestrictedRoute/RestrictedRoute";
+import { PrivateRoute } from "../components/PrivateRoute/PrivateRoute";
+import { Toaster } from "react-hot-toast";
+import { DNA } from "react-loader-spinner";
+
+function App() {
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <div className="app_loader">
+      <b>Refreshing user...</b>
+      <DNA
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+      />
+    </div>
+  ) : (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegistrationPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+      </Routes>
+      <Toaster />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import s from "./HomePage.module.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <p className={s.homepage}>
+        Register and use your phone book whenever you need it!!
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
